b28e914a6c31c9e4be7b52abd4473cb44eea77e6
ekegrant59 <107748298+ekegrant59@users.noreply.github.com>
2023-08-07 08:43:56 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require('express') 
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const session = require('express-session')
+
+const app = express()
+app.use('/assets', express.static('assets')) 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(express.json())
+app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      secret: 'secret',
+    })
+);
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.get('/', function(req,res){ 
+    res.render('index')
+})
+app.get('/about', function(req,res){ 
+    res.render('about')
+})
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`App started on port ${port}`)
+} )
